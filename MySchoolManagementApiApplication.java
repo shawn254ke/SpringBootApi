bdbb515b6d98a7fb05e34574b181b8fcb3f9df04
shawn254ke <131768937+shawn254ke@users.noreply.github.com>
2024-12-10 19:00:24 +0300

    Add files via upload

---
+++ MySchoolManagementApiApplication.java
@@ -0,0 +1,13 @@
+package app.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchoolManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySchoolManagementApiApplication.class, args);
+	}
+
+}
